2367af3b26641c42f742b06585a33c170a3dbd2e
Lorens León <lorensleon@gmail.com>
2024-10-12 01:07:06 +0200

    feat: replase `chai` by `node:assert`

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,17 +1,17 @@
+import assert from 'node:assert';
 import { describe, it } from 'node:test';
-import { expect } from 'chai';
 import { sum } from './sum.js';
 
 describe('sum()', () => {
   it('should sum two values', () => {
-    expect(sum(1, 2)).to.be.equals(1 + 2);
+    assert.equal(sum(1, 2), 3);
   });
 
   it('should sum three values', () => {
-    expect(sum(1, 2, 3)).to.be.equals(1 + 2 + 3);
+    assert.equal(sum(1, 2, 3), 6);
   });
 
   it('should sum negative values', () => {
-    expect(sum(1, -1)).to.be.equal(0);
+    assert.equal(sum(1, -1), 0);
   });
 });
